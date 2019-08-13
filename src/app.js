7df899d17ecbd22ff1ceed3327614412641292fc
Shahrair701 <shahriar701@gmail.com>
2019-08-13 18:25:40 +0600

    Fixed port for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -97,5 +97,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port' + port)
+    console.log('Server is up on port ' + port)
 })
